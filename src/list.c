142d1c29987138d546b81863327eebf6a4d9b266
Apoorv Kothari <apoorv@toidiu.com>
2024-12-17 22:34:23 -0800

    cmake proj init

---
+++ src/list.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int print_list() {
+    // printf() displays the string inside quotation
+    printf("Hello, World!");
+
+    int arr[] = {1, 2, 3};
+    /* int arr[] = {0}; */
+    /* arr[] = {0}; */
+
+    return 0;
+}
+
